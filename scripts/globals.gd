33691b730f3243ecab737745e8ffc2ffd895e2ff
Raulios <raulmolina88@gmail.com>
2023-10-21 00:36:34 +0200

    switch turned on and off logic, kid to stop if all lights up

---
+++ scripts/globals.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var all_lights_up: bool = false
---
+++ scripts/switch.gd
@@ -0,0 +1,26 @@
+extends StaticBody2D
+
+signal on_off_change(is_turned_on)
+
+var is_turned_on: bool = false
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func turn_on():
+	if not is_turned_on:
+		is_turned_on = true
+		on_off_change.emit(is_turned_on)
+	
+func turn_off():
+	if is_turned_on:
+		is_turned_on = false
+		on_off_change.emit(is_turned_on)
